e73dc9c48b5d98df069c76f3551f691f3d33baa4
lcr <lcr@melexis.com>
2019-08-29 13:43:29 +0200

    add equation evaluation for step and cap analogue bricks

--- pywave/analogue.py
+++ pywave/analogue.py
@@ -144,9 +144,9 @@ def generate_analogue_symbol(symbol: str, **kwargs) -> (bool, object):
         block = Meta(**kwargs)
     # full custom analogue bloc
     elif symbol == pywave.BRICKS.step:
-        block = Step(pywave.BRICKS.transform_y(float(equation), height), **kwargs)
+        block = Step(pywave.BRICKS.transform_y(eval(equation, CONTEXT), height), **kwargs)
     elif symbol == pywave.BRICKS.cap:
-        block = Cap(pywave.BRICKS.transform_y(float(equation), height), **kwargs)
+        block = Cap(pywave.BRICKS.transform_y(eval(equation, CONTEXT), height), **kwargs)
     elif symbol == pywave.BRICKS.ana:
         last_y = height if last_y is None else last_y
         try:
